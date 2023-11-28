98121f56ecda121a8b4a7b6cd5fe17a19201c0ad
Harnoor <gill_h@pm.me>
2023-11-28 15:25:08 +1300

    added functionality for creating tickets;

---
+++ ticketsystem_frontend/src/components/NewTicket.tsx
@@ -0,0 +1,130 @@
+import { Autocomplete, Box, Button, Grid, Paper, TextField, Typography } from '@mui/material';
+import { ChangeEvent, FormEvent, useState } from 'react';
+
+
+interface FormProps {
+    showCard: boolean,
+    onClose: () => void,
+}
+
+interface TicketData {
+    title: string,
+    content: string,
+    status: number,
+}
+
+interface statusDict {
+    [key: string]: number,
+}
+
+const statusOptions: statusDict = { 'To Do': 0, 'In Progress' : 1, 'Done' : 2 };
+
+export default function NewTicket({ showCard, onClose }: FormProps) {
+    const [formData, setFormData] = useState<TicketData>({
+        title: '',
+        content: '',
+        status: 0,
+    });
+    const optionsArr = Object.keys(statusOptions);
+    const [optionsValue, setOptionsValue] = useState<string | null>(optionsArr[0]);
+
+    const handleFormChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target;
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
+    };
+
+    const handleSubmit = async (event: FormEvent) => {
+        event.preventDefault();
+        formData.status = statusOptions[optionsValue === null ? 'To Do' : optionsValue];
+        try {
+            const response = await fetch('https://localhost:7278/api/tickets', {
+                method: 'POST',
+                body: JSON.stringify(formData),
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+
+            if (!response.ok) {
+                throw new Error(`Error! status: ${response.status}`);
+            }
+
+        } catch (error) {
+            if (error instanceof Error) {
+                console.log('error message: ', error.message);
+                return error.message;
+            } else {
+                console.log('unexpexted error: ', error);
+                return 'An unexpected error occurred';
+            }
+        }
+        onClose();
+    };
+
+
+    return (
+        <Box sx={{
+            position: 'absolute',
+            width: '100%',
+            height: '107%',
+            bgcolor: 'rgba(0,0,0,0.54)',
+            colour: 'white',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+        }}>
+            <Paper variant="outlined" sx={{width: '30%', my: { xs: 3, md: 6 }, p: { xs: 2, md: 2 } }}>
+                <Typography variant="h6" gutterBottom>
+                    New Ticket
+                </Typography>
+                    <Grid container spacing={3}>
+                        <Grid item xs={12}>
+                            <TextField
+                                required
+                                id="title"
+                                name="title"
+                                label="Title"
+                                fullWidth
+                                autoComplete="ticket-title"
+                                value={formData.title}
+                                onChange={handleFormChange}
+                            />
+                        </Grid>
+                        <Grid item xs={12} >
+                            <TextField
+                                required
+                                id="content"
+                                name="content"
+                                label="Description"
+                                fullWidth
+                                autoComplete="ticket-content"
+                                multiline
+                                rows={5}
+                                value={formData.content}
+                                onChange={handleFormChange}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Autocomplete
+                                value={optionsValue}
+                                onChange={(event: any, newValue: string | null) => {
+                                    setOptionsValue(newValue);
+                                }}
+                                options={optionsArr}
+                                sx={{ width: 300 }}
+                                renderInput={(params) => <TextField required {...params} label="Status" />}
+                            />
+                        </Grid>
+                    </Grid>
+                <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+                    <Button sx={{ m: 1 }} variant='contained' type="submit" onClick={handleSubmit} >Create Ticket</Button>
+                    <Button sx={{ m: 1 }} variant='outlined' onClick={onClose}>Close</Button>
+                </Box>
+            </Paper>
+
+        </Box>
+    )
+}
---
+++ ticketsystem_frontend/src/components/TicketDashboard.tsx
@@ -0,0 +1,31 @@
+import { Box, Button, Grid, Paper } from '@mui/material';
+import React, { useState } from 'react';
+import GetTickets from './GetTickets';
+import NewTicket from './NewTicket';
+
+
+
+export default function TicketDashboard() {
+    const [showNewTicketCard, setShowNewTicketCard] = useState(false);
+
+    return (
+        <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+            <Grid container>
+                <Grid item xs={12} >
+                    <Box>
+                        <Button sx={{ m:2 }} variant="outlined" onClick={() => setShowNewTicketCard(true)}>
+                            New Ticket
+                        </Button>
+
+                    </Box>
+                    <Paper sx={{ p:2, display: 'flex', flexDirection: 'column', overflow: 'auto'}}>
+
+                        <GetTickets />
+
+                    </Paper>
+                </Grid>
+            </Grid>
+            {showNewTicketCard && <NewTicket showCard={showNewTicketCard} onClose={() => setShowNewTicketCard(false)} />}
+        </Box>
+    );
+}
